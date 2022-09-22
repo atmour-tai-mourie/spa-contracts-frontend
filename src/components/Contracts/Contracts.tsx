17e4b43177ab1e869380db00eded26fdd6f11c3f
atmour-power <tai@powerfinance.co.nz>
2022-09-23 11:44:31 +1200

    add basic contracts

--- src/components/Contracts/Contracts.tsx
+++ src/components/Contracts/Contracts.tsx
@@ -1,7 +1,42 @@
-import * as React from "react";
+import { Card } from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+interface Contract {
+  contractID: string;
+  contractAmount: number;
+  interestRate: number;
+  borrower: string;
+  investor: string;
+}
 
 const Contracts: React.FC = () => {
-  return <></>;
+  const [contracts, setContracts] = useState<Contract[]>([]);
+  const fetchContractData = async () => {
+    const contractsResponse = await fetch(
+      "http://localhost:7071/api/HttpTrigger2"
+    );
+
+    const contractsData = await contractsResponse.json();
+    console.log(contractsData);
+
+    setContracts(contractsData);
+  };
+
+  useEffect(() => {
+    fetchContractData().catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
+  return (
+    <>
+      {contracts.map((contract) => (
+        <Card>
+          <h3>{contract.contractAmount}</h3>
+        </Card>
+      ))}
+    </>
+  );
 };
 
 export default Contracts;
