225a8900038bc082d7d7724aee6bc9cfd659f0da
atmour-power <tai@powerfinance.co.nz>
2022-09-23 12:19:46 +1200

    add header links

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 import classes from "./Header.module.css";
 
@@ -6,6 +7,12 @@ const Header: React.FC = () => {
   return (
     <header className={classes.header}>
       <h1>PF Contracts</h1>
+      <Link to="/" className={classes.link}>
+        Add New Contract
+      </Link>
+      <Link to="/contracts" className={classes.link}>
+        Contracts
+      </Link>
     </header>
   );
 };
